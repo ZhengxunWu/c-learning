971702c6159e8d1cf8f1163de5cbdcbd0b7fb792
zhengxunWu <zw227@cornell.edu>
2019-12-29 21:56:19 -0500

    .

---
+++ color.pl
@@ -0,0 +1,7 @@
+color(red).
+color(green).
+color(blue).
+colorify(A,B,C,D,E) :-
+    color(A), color(B), color(C), color(D), color(E),
+    \+ A=B, \+ A=C, \+ A=D, \+ A=E,
+    \+ B=C, \+ C=D, \+ D=E.
